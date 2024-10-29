6fd8ced6e7a5630a8fda520f65d50a3d07052b27
Rumbling <89301632+cennedpine@users.noreply.github.com>
2024-10-29 21:06:36 +0800

    Update reserve.py

--- utils/reserve.py
+++ utils/reserve.py
@@ -60,7 +60,7 @@ class reserve:
         response = self.requests.get(url=url, verify=False)
         html = response.content.decode('utf-8')
         token = re.findall(
-            'token: \'(.*?)\'', html)[0] if len(re.findall('token: \'(.*?)\'', html)) > 0 else ""
+           r"token: '([^']*)'", html)[0] if re.findall(r"token: '([^']*)'", html) else ""
         return token
 
     def get_login_status(self):
